878affbdc746b4d703e92840ecd880f8847d7fda
jialei132 <106511333+jialei132@users.noreply.github.com>
2022-05-31 12:49:36 +0800

    Update Demo02.java

--- git-demo/src/com/offcn/demos/Demo02.java
+++ git-demo/src/com/offcn/demos/Demo02.java
@@ -4,5 +4,6 @@ public class Demo02 {
     public static void main(String[] args) {
         System.out.println("EEEEEEEEE");
         System.out.println("FFFFFFFFF");
+        System.out.println("认真的");
     }
 }
