167b0715e0afc3698968e95981512004dad08a74
jialei132 <106511333+jialei132@users.noreply.github.com>
2022-05-31 12:23:33 +0800

    Update Demo.java

--- git-demo/src/com/offcn/demos/Demo.java
+++ git-demo/src/com/offcn/demos/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("HelloWord!");
         System.out.println("AAAAAAAAAAAA");
+        System.out.println("BBBBBBBBBB");
     }
 }
