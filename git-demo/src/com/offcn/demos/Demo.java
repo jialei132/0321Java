a1342f2310f26939fe44b17377e114a014092195
Ace <1694601613@qq.com>
2022-05-31 13:00:50 +0800

    合并了test分支的操作

--- git-demo/src/com/offcn/demos/Demo.java
+++ git-demo/src/com/offcn/demos/Demo.java
@@ -1,6 +1,6 @@
 package com.offcn.demos;
 
-public class Demo01 {
+public class Demo {
     public static void main(String[] args) {
         System.out.println("HelloWord!");
         System.out.println("AAAAAAAAAAAA");
